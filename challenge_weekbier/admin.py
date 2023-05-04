7eb2a20d6df98ac8d4e22a4e80851572302fc9b6
Alex <hartog.alex@gmail.com>
2023-05-04 11:35:35 +0200

    Initial commit

---
+++ challenge_weekbier/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+
+from .models import Player, Checkin
+
+admin.site.register(Player)
+admin.site.register(Checkin)
---
+++ challenge_weekbier/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+
+
+class Player(models.Model):
+    """A player participating in the challenge."""
+    name = models.CharField(max_length=200)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        """Return a string representation of the model."""
+        return self.name
+
+
+class Checkin(models.Model):
+    """A checkin for a player."""
+    player = models.ForeignKey(Player, on_delete=models.CASCADE)
+    date = models.DateField()
+    place = models.CharField(max_length=200)
+    city = models.CharField(max_length=200)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        """Return a string representation of the model."""
+        return f"{self.player.name} checked in at {self.date_added}"
