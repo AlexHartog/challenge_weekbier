2562b529b8a590bc03f20799e3cab829a429c304
Alex Hartog <hartog.alex@gmail.com>
2023-10-12 13:48:25 +0200

    Added DEBUG_MODE as .env variable

--- app/challenge_weekbier/settings.py
+++ app/challenge_weekbier/settings.py
@@ -31,8 +31,7 @@ assert os.getenv(
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("DEBUG_MODE", False) == "True"
 
 # TODO: Make this more proper?
 ALLOWED_HOSTS = [
